c139f8510edc198188cff17f943fe2ab4de59113
Fergus Strange <fergus.strange@infinityworks.com>
2019-04-25 20:39:57 +1000

    Add material UI.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing to see if this deploys
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const App = () => (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+);
 
 export default App;

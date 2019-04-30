db68665653e39d58a20a7c291e6f4e1104b7d26d
Fergus Strange <fergus.strange@infinityworks.com>
2019-04-30 16:22:56 +1000

    Move to using dto for return round

--- api/handlers/read/read.go
+++ api/handlers/read/read.go
@@ -6,8 +6,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Round struct {
+	Url          string        `json:"url"`
+	Participants []Participant `json:"participants"`
+}
+
+type Participant struct {
+	UUID       string `json:"uuid"`
+	Name       string `json:"name"`
+	RoundCount int    `json:"round_count"`
+}
+
 func GetRound(ctx *gin.Context) {
 	roundId := validation.ValidRoundId(ctx)
 	round := persistence.FetchRound(roundId)
-	ctx.JSON(200, &round)
+	ctx.JSON(200, transform(round))
+}
+
+func transform(round persistence.Round) *Round {
+	var participants []Participant
+	for _, participant := range round.Participants {
+		participants = append(participants, Participant{
+			UUID:       participant.UUID,
+			Name:       participant.Name,
+			RoundCount: participant.RoundCount,
+		})
+	}
+
+	return &Round{
+		Url:          round.Url,
+		Participants: participants,
+	}
 }
